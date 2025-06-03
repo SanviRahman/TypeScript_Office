ab157bdc20ce4e259819dac0777138861882e144
Sanvi Rahman <SanviRahman>
2025-06-03 15:16:43 +0600

    behh

---
+++ Basic/generic.js
@@ -0,0 +1,5 @@
+var printUserInfo = function (userID, age) {
+    console.log("User ID: ".concat(userID, " and Age: ").concat(age));
+};
+printUserInfo(101, 24);
+printUserInfo("101", 24);
---
+++ Basic/generic.ts
@@ -0,0 +1,5 @@
+const printUserInfo = <X,Y>(userID:X , age: Y) =>{
+    console.log(`User ID: ${userID} and Age: ${age}`);
+}
+printUserInfo(101,24);
+printUserInfo("101",24);
