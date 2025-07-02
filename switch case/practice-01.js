b5127a55b424fb8a2cadaa199440a515c3e1cf4b
Sanvi Rahman <SanviRahman>
2025-07-02 12:27:55 +0600

    last commit

---
+++ switch case/practice-01.js
@@ -0,0 +1,11 @@
+var tergetId = '';
+switch (tergetId) {
+    case 'btnUpdate':
+        console.log("Update");
+        break;
+    case 'btnDelete':
+        console.log("Delete");
+        break;
+    default:
+        console.log("btnUnknown");
+}
---
+++ switch case/practice-01.ts
@@ -0,0 +1,12 @@
+let tergetId:string='';
+
+switch(tergetId){
+    case 'btnUpdate':
+        console.log("Update");
+        break;
+    case 'btnDelete':
+        console.log("Delete");
+        break;
+    default:
+        console.log("btnUnknown");
+}
